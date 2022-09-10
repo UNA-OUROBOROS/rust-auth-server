51524f25b3102b1ad5414c2946976d50cc995d83
Jonathan Guzmán <jonguz5@hotmail.com>
2022-09-10 08:53:18 -0600

    test for invalid chars

--- auth-server-lib/src/tests/config.rs
+++ auth-server-lib/src/tests/config.rs
@@ -1,11 +1,12 @@
-
-use crate::dboilerplate::configuration::{
-    is_valid_identifier
-};
-
-
+use crate::dboilerplate::configuration::is_valid_identifier;
 
 #[test]
 fn test_is_valid_identifier() {
     assert_eq!(is_valid_identifier("valid_name"), true);
+    static INVALID_CHARS: [char; 15] = [
+        '\\', '/', '?', '%', '*', ':', '|', '"', '<', '>', '.', ',', ';', '=', ' ',
+    ];
+    for c in INVALID_CHARS.iter() {
+        assert_eq!(is_valid_identifier(&format!("invalid{}", c)), false);
+    }
 }
