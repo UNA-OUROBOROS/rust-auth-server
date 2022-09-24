dceaae9e9fad6df69dc4562067c8d91e474b9902
Jonathan Guzmán <jonguz5@hotmail.com>
2022-09-23 21:43:00 -0600

    removed alias support the alias support should be on the application itself

--- token-helper/src/user.rs
+++ token-helper/src/user.rs
@@ -6,9 +6,6 @@ pub struct UserData {
     // the user realm where he comes from (so the user can have the same id in different realms)
     // however the user id is unique across all realms
     pub realm: String,
-    // used if this user is an alias of another user(ex: the user was mapped from 2fa to an existing user)
-    // accepted values '=' for false and ~ for true
-    pub is_alias: bool,
 }
 
 impl UserData {
@@ -20,13 +17,13 @@ impl UserData {
     /// ```
     /// use token_helper::user::UserData;
     ///
-    /// let user_data = UserData::new("1234567890abcdef1234567890abcdef12345678".to_string(), "test-realm".to_string(), false);
+    /// let user_data = UserData::new("1234567890abcdef1234567890abcdef12345678".to_string(), "test-realm".to_string());
     /// println!("{:?}", user_data);
     /// assert!(user_data.is_ok());
-    /// let invalid_user_data = UserData::new("invalid_user".to_string(), "test-realm".to_string(), true);
+    /// let invalid_user_data = UserData::new("invalid_user".to_string(), "test-realm".to_string());
     /// assert!(invalid_user_data.is_err());
     /// ```
-    pub fn new(user_id: String, realm: String, is_alias: bool) -> Result<Self, String> {
+    pub fn new(user_id: String, realm: String) -> Result<Self, String> {
         if user_id.len() < 36 {
             return Err("user id must be at least 36 characters long".to_string());
         }
@@ -53,11 +50,7 @@ impl UserData {
             return Err("realm cannot contain 2 dots next to each other".to_string());
         }
 
-        Ok(Self {
-            user_id,
-            realm,
-            is_alias,
-        })
+        Ok(Self { user_id, realm })
     }
 
     /// constructs the user data from a subject string
@@ -66,42 +59,30 @@ impl UserData {
     /// ```
     /// use token_helper::user::UserData;
     ///
-    /// let subject = "=:1234567890abcdef1234567890abcdef12345678:example.com";
+    /// let subject = "1234567890abcdef1234567890abcdef12345678:example.com";
     /// let user_data = UserData::from_subject(subject).unwrap();
     /// assert_eq!(user_data.user_id, "1234567890abcdef1234567890abcdef12345678");
     /// assert_eq!(user_data.realm, "example.com");
-    /// assert_eq!(user_data.is_alias, false);
     /// ```
     ///
     pub fn from_subject(subject: &str) -> Result<Self, String> {
         let mut parts = subject.split(':');
-        // the alias can be = or ~
-        let is_alias = match parts.next().ok_or("invalid alias")? {
-            "=" => false,
-            "~" => true,
-            _ => return Err("invalid alias".to_string()),
-        };
         let user_id = parts.next().ok_or("invalid subject")?.to_string();
         let realm = parts.next().ok_or("invalid subject")?.to_string();
-        Self::new(user_id, realm, is_alias)
+        Self::new(user_id, realm)
     }
 
     /// returns the subject in the following format:
-    /// {is_alias}:{user_id}:{realm} 1234567890abcdef1234567890abcdef12345678:example.com
+    /// {user_id}:{realm} 1234567890abcdef1234567890abcdef12345678:example.com
     ///
     /// #example
     /// ```
     /// use token_helper::user::UserData;
     ///
-    /// let user_data = UserData::new("1234567890abcdef1234567890abcdef12345678".to_string(), "example.com".to_string(), false).unwrap();
-    /// assert_eq!(user_data.get_subject(), "=:1234567890abcdef1234567890abcdef12345678:example.com");
+    /// let user_data = UserData::new("1234567890abcdef1234567890abcdef12345678".to_string(), "example.com".to_string()).unwrap();
+    /// assert_eq!(user_data.get_subject(), "1234567890abcdef1234567890abcdef12345678:example.com");
     /// ```
     pub fn get_subject(&self) -> String {
-        format!(
-            "{}:{}:{}",
-            if self.is_alias { "~" } else { "=" },
-            self.user_id,
-            self.realm,
-        )
+        format!("{}:{}", self.user_id, self.realm,)
     }
 }
